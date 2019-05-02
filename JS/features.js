b56a6d7cb3af107852909f03b8d558cc145d7953
RyanSWest <west56309@gmail.com>
2019-05-01 23:30:19 -0500

    more features added

---
+++ JS/features.js
@@ -0,0 +1,12 @@
+
+let signUp1 = document.querySelector('#button1')
+ 
+let signUp2 = document.querySelector('#button2')
+
+signUp1.addEventListener('click', function(){
+     location.replace("signUp.html");})
+
+signUp2.addEventListener('click', function(){
+     location.replace("signUp.html");})
+
+      
